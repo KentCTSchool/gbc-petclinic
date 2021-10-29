02a9dc41fa5f0330d650a47f9d63271ce2bc7f8b
Kent Pedrocha <101266723@georgebrown.ca>
2021-10-29 14:37:17 -0400

    first commit

---
+++ gbc-petclinic/src/main/java/ca/gbc/comp3095/gbcpetclinic/GbcPetclinicApplication.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.gbcpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbcPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbcPetclinicApplication.class, args);
+    }
+
+}
---
+++ gbc-petclinic/src/test/java/ca/gbc/comp3095/gbcpetclinic/GbcPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.gbcpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbcPetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
