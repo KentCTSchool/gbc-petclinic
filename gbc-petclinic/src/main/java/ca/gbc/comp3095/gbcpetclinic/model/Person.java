ab77209ea434da347afe23cb75794e724c08076d
Kent Pedrocha <101266723@georgebrown.ca>
2021-10-29 16:35:54 -0400

    updates

--- gbc-petclinic/src/main/java/ca/gbc/comp3095/gbcpetclinic/model/Person.java
+++ gbc-petclinic/src/main/java/ca/gbc/comp3095/gbcpetclinic/model/Person.java
@@ -1,4 +1,23 @@
 package ca.gbc.comp3095.gbcpetclinic.model;
 
 public class Person {
+
+    private String first_name;
+    private String last_name;
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
 }
