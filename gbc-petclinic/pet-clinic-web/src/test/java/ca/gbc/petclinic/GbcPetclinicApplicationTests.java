11a00a313f36430ee323f971a8bb608fbac1ca7c
Kent Pedrocha <101266723@georgebrown.ca>
2021-11-02 09:41:04 -0400

    Add Maven Release Plugins

---
+++ gbc-petclinic/pet-clinic-web/src/test/java/ca/gbc/petclinic/GbcPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbcPetclinicApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
